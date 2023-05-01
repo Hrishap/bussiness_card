c3338a4f9e7324e844891a907374f67aad753372
Hrishap <121787992+Hrishap@users.noreply.github.com>
2023-05-01 20:50:01 +0530

    change in profile picture

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,10 +2,11 @@ import React from "react";
 import profile from "../assets/profile.png";
 import email from "../assets/email.png";
 import linkedIn from "../assets/linkedIn.png";
+import PROFILE from "../assets/PROFILE-gif.gif";
 function Info(){
     return(
         <div>
-         <img src={profile} alt="profile" className="profile-picture"/>
+         <img src={PROFILE} alt="profile" className="profile-picture"/>
          <h1 className="Name">Hrishap Joshi</h1>
          <h4 className="designation">Frontend Developer</h4>
          <p className="website">hrishapjoshi.website</p>
